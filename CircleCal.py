492977fd1657e935c6fff0c448b816af609da470
ggwplg7 <jymmnn7@gmail.com>
2025-03-19 22:33:28 +0900

    Add files via upload

---
+++ CircleCal.py
@@ -0,0 +1,6 @@
+radius = float(input("원의 반지름을 입력하세요: "))
+pi = 3.141592
+circumference = 2 * pi * radius  # 원의 둘레
+area = pi * radius ** 2  # 원의 넓이
+print("원의 둘레:", circumference)
+print("원의 넓이:", area)
---
+++ inchTocm.py
@@ -0,0 +1,3 @@
+inch = float(input("inch 단위를 입력하세요: "))
+cm = inch * 2.54
+print("cm 단위로 변환한 값:", cm)
